952390a62dcf4590649c0104ca3cd8ff628d21c5
junslog <wonjon0930@daum.net>
2024-01-28 11:48:33 +0900

    [Gold II] Title: 후위 표기식, Time: 108 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Main {
+    static int precedence(char ch) {
+        if (ch == '(') return 0;
+        if (ch == '+' || ch == '-') return 1;
+        else return 2;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String ans = "";
+        Stack<Character> ops = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if ('A' <= ch && ch <= 'Z') {
+                ans += ch;
+            } else if (ch == '(') {
+                ops.push(ch);
+            } else if (ch == ')') {
+                while (!ops.isEmpty()) {
+                    char op = ops.pop();
+                    if (op == '(') break;
+                    ans += op;
+                }
+            } else {
+                while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(ch)) {
+                    ans += ops.pop();
+                }
+                ops.push(ch);
+            }
+        }
+        while (!ops.isEmpty()) {
+            ans += ops.pop();
+        }
+        System.out.println(ans);
+    }
+}
