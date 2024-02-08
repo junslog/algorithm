77e24fff4364f1d7e34cba2f206c6ad4d7a7f18f
junslog <wonjon0930@daum.net>
2024-02-08 15:12:57 +0900

    [Silver III] Title: N과 M (6), Time: 80 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] items;
+    static int[] input;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        input = new int[N];
+        for(int i = 0; i < N; i++){
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+        items = new int[M];
+        backtracking(0,0);
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static void backtracking(int depth, int start){
+        if(depth == M){
+            for(int i = 0; i < M; i++){
+                sb.append(items[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < N; i++){
+            items[depth] = input[i];
+            backtracking(depth+1, i+1);
+        }
+    }
+}
