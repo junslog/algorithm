42f33ae3690955f994db141f8d976ac597ad0a0c
junslog <wonjon0930@daum.net>
2024-02-16 15:00:42 +0900

    [Silver V] Title: 집합, Time: 1656 ms, Memory: 345276 KB -BaekjoonHub

--- 백준/Silver V/11723. 집합/집합.java
+++ 백준/Silver V/11723. 집합/집합.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,43 +8,39 @@ public class Main {
         int M = Integer.parseInt(br.readLine());
         String command, cmd;
         int oper;
-        Set<Integer> set = new HashSet<>();
+        boolean[] set = new boolean[21];
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             command = br.readLine();
             if (command.equals("all") || command.equals("empty")) {
                 if (command.equals("all")) {
                     for (int item = 1; item <= 20; item++) {
-                        set.add(item);
+                        set[item] = true;
                     }
                 }
                 if (command.equals("empty")) {
                     for (int item = 1; item <= 20; item++) {
-                        set.remove(item);
+                        set[item] = false;
                     }
                 }
             } else {
                 cmd = command.split(" ")[0];
                 oper = Integer.parseInt(command.split(" ")[1]);
                 if (cmd.equals("add")) {
-                    set.add(oper);
+                    set[oper] = true;
                 }
                 if (cmd.equals("remove")) {
-                    set.remove(oper);
+                    set[oper] = false;
                 }
                 if (cmd.equals("check")) {
-                    if (set.contains(oper)) {
+                    if (set[oper]) {
                         sb.append(1).append("\n");
                     } else {
                         sb.append(0).append("\n");
                     }
                 }
                 if (cmd.equals("toggle")) {
-                    if (set.contains(oper)) {
-                        set.remove(oper);
-                    } else {
-                        set.add(oper);
-                    }
+                    set[oper] = !set[oper];
                 }
             }
         }
