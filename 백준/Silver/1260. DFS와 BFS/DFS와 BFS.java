d9a5b4b2687bcf3e3b299de36506175a6e3c6492
junslog <wonjon0930@daum.net>
2024-02-21 14:11:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 176 ms, Memory: 17132 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,58 +1,78 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] a;
-    static boolean[] c;
-    public static void dfs(int x) {
-        if (c[x]) {
-            return;
+    static int N, M, V;
+    static List<Integer>[] adjList;
+    static StringBuilder dfsSb = new StringBuilder();
+    static StringBuilder bfsSb = new StringBuilder();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        adjList = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
         }
-        c[x] = true;
-        System.out.print(x + " ");
-        for (int y : a[x]) {
-            if (c[y] == false) {
-                dfs(y);
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            adjList[from].add(to);
+            adjList[to].add(from);
+        }
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(adjList[i]);
+        }
+        visited[V] = true;
+        dfsSb.append(V).append(" ");
+        dfs(V);
+        for (int i = 1; i <= N; i++) {
+            visited[i] = false;
+        }
+        bfs();
+        System.out.println(dfsSb);
+        System.out.println(bfsSb);
+    }
+
+    public static void dfs(int curr) {
+        for (int i : adjList[curr]) {
+            if (visited[i]) {
+                continue;
             }
+            visited[i] = true;
+            dfsSb.append(i).append(" ");
+            dfs(i);
         }
     }
-    public static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(start);
-        c[start] = true;
+
+    public static void bfs() {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(V);
+        visited[V] = true;
         while (!q.isEmpty()) {
-            int x = q.remove();
-            System.out.print(x + " ");
-            for (int y : a[x]) {
-                if (c[y] == false) {
-                    c[y] = true;
-                    q.add(y);
+            int curr = q.poll();
+            bfsSb.append(curr).append(" ");
+            for (int i : adjList[curr]) {
+                if (visited[i]) {
+                    continue;
                 }
+                visited[i] = true;
+                q.add(i);
             }
         }
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int start = sc.nextInt();
-        a = (ArrayList<Integer>[]) new ArrayList[n+1];
-        for (int i=1; i<=n; i++) {
-            a[i] = new ArrayList<Integer>();
-        }
-        for (int i=0; i<m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            a[u].add(v);
-            a[v].add(u);
-        }
-        for (int i=1; i<=n; i++) {
-            Collections.sort(a[i]);
-        }
-        c = new boolean[n+1];
-        dfs(start);
-        System.out.println();
-        c = new boolean[n+1];
-        bfs(start);
-        System.out.println();
     }
 }
