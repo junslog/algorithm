8a570505856d6bda125203e81437f2702b36c0e2
junslog <wonjon0930@daum.net>
2024-02-22 19:09:32 +0900

    [Gold IV] Title: 이분 그래프, Time: 1124 ms, Memory: 286416 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,50 +1,70 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void dfs(ArrayList<Integer>[] a, int[] color, int x, int c) {
-        color[x] = c;
-        for (int y : a[x]) {
-            if (color[y] == 0) {
-                dfs(a, color, y, 3-c);
+
+    static List<Integer>[] adjList;
+    static int[] color;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < K; t++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            adjList = new ArrayList[N];
+            for (int i = 0; i < N; i++) {
+                adjList[i] = new ArrayList<>();
             }
-        }
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            ArrayList<Integer>[] a = (ArrayList<Integer>[]) new ArrayList[n+1];
-            for (int i=1; i<=n; i++) {
-                a[i] = new ArrayList<Integer>();
+            color = new int[N];
+
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int source = Integer.parseInt(st.nextToken()) - 1;
+                int dest = Integer.parseInt(st.nextToken()) - 1;
+                adjList[source].add(dest);
+                adjList[dest].add(source);
             }
-            for (int i=0; i<m; i++) {
-                int u = sc.nextInt();
-                int v = sc.nextInt();
-                a[u].add(v);
-                a[v].add(u);
-            }
-            int[] color = new int[n+1];
-            boolean ok = true;
-            for (int i=1; i<=n; i++) {
-                if (color[i] == 0) {
-                    dfs(a, color, i, 1);
+
+            for (int j = 0; j < N; j++) {
+                if (color[j] == 0) {
+                    dfs(j, 1);
                 }
             }
-            for (int i=1; i<=n; i++) {
-                for (int j : a[i]) {
-                    if (color[i] == color[j]) {
-                        ok = false;
+
+            boolean isBiPartitie = true;
+            for (int i = 0; i < N; i++) {
+                for (int elem : adjList[i]) {
+                    if (color[elem] == color[i]) {
+                        isBiPartitie = false;
+                        break;
                     }
                 }
             }
-            if (ok) {
-                System.out.println("YES");
+
+            if (isBiPartitie) {
+                sb.append("YES").append("\n");
             } else {
-                System.out.println("NO");
+                sb.append("NO").append("\n");
             }
         }
+        System.out.println(sb);
+    }
 
+    public static void dfs(int curr, int c) {
+        color[curr] = c;
+        for (int elem : adjList[curr]) {
+            if (color[elem] == 0) {
+                dfs(elem, 3 - c);
+            }
+        }
     }
 }
