78b37d9fdd20d77100f8206fea9a6637ae98d2fc
junslog <wonjon0930@daum.net>
2024-02-23 02:20:15 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,7 +1,15 @@
+# SELECT
+#     ANIMAL_ID,
+#     NAME,
+#     CASE WHEN (SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%') THEN 'O'
+#     ELSE 'X' END '중성화'
+# FROM
+#     ANIMAL_INS
+    
+    
 SELECT
     ANIMAL_ID,
     NAME,
-    CASE WHEN (SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%') THEN 'O'
-    ELSE 'X' END '중성화'
+    IF(SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%', 'O', 'X') as '중성화'
 FROM
     ANIMAL_INS
