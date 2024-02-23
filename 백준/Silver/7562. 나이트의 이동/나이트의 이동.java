6f25bbbbfae0bbcfeddc68b38a20f8fcb9e55b22
junslog <wonjon0930@daum.net>
2024-02-24 01:54:59 +0900

    [Silver I] Title: 나이트의 이동, Time: 240 ms, Memory: 63288 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,38 +1,88 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-    static final int[] dx = {-2,-1,1,2,2,1,-1,-2};
-    static final int[] dy = {1,2,2,1,-1,-2,-2,-1};
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            int sx = sc.nextInt();
-            int sy = sc.nextInt();
-            int ex = sc.nextInt();
-            int ey = sc.nextInt();
-            int[][] d = new int[n][n];
-            for (int i=0; i<n; i++) {
-                Arrays.fill(d[i],-1);
+
+    static int[] dx = {2, 1, -1, -2, -2, -1, 1, 2};
+    static int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
+    static int boardSize;
+    static int destX, destY;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            boardSize = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int startX = Integer.parseInt(st.nextToken());
+            int startY = Integer.parseInt(st.nextToken());
+            Position knightStartP = new Position(startX, startY);
+
+            st = new StringTokenizer(br.readLine());
+            destX = Integer.parseInt(st.nextToken());
+            destY = Integer.parseInt(st.nextToken());
+            visited = new boolean[boardSize][boardSize];
+            if (startX == destX && startY == destY) {
+                sb.append(0).append("\n");
+            } else {
+                int minTrial = bfs(knightStartP);
+                sb.append(minTrial).append("\n");
             }
-            Queue<Integer> q = new LinkedList<>();
-            q.add(sx); q.add(sy);
-            d[sx][sy] = 0;
-            while (!q.isEmpty()) {
-                int x = q.remove();
-                int y = q.remove();
-                for (int k=0; k<8; k++) {
-                    int nx = x+dx[k];
-                    int ny = y+dy[k];
-                    if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-                        if (d[nx][ny] == -1) {
-                            d[nx][ny] = d[x][y] + 1;
-                            q.add(nx); q.add(ny);
-                        }
+        }
+        System.out.println(sb);
+    }
+
+    public static int bfs(Position p) {
+        Queue<Position> q = new LinkedList<>();
+        q.add(p);
+        int qSize = q.size();
+        visited[p.x][p.y] = true;
+        int trial = 0;
+        while (!q.isEmpty()) {
+            for (int t = 0; t < qSize; t++) {
+                Position elem = q.poll();
+                for (int i = 0; i < 8; i++) {
+                    int nx = elem.x + dx[i];
+                    int ny = elem.y + dy[i];
+
+                    if (nx < 0 || ny < 0 || nx >= boardSize || ny >= boardSize) {
+                        continue;
                     }
+
+                    if (visited[nx][ny]) {
+                        continue;
+                    }
+
+                    if (nx == destX && ny == destY) {
+                        trial++;
+                        return trial;
+                    }
+                    visited[nx][ny] = true;
+                    q.add(new Position(nx, ny));
                 }
             }
-            System.out.println(d[ex][ey]);
+            qSize = q.size();
+            trial++;
         }
+        return trial;
+    }
+
+
+    public static class Position {
+        int x;
+        int y;
+
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
     }
 }
