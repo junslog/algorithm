31960e43693835be6383295b412e7a1d2a25ff7f
junslog <wonjon0930@daum.net>
2024-02-24 12:00:49 +0900

    [level 3] Title: 대여 기록이 존재하는 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -0,0 +1,25 @@
+-- 세단인 종류 자동차 아이디 추출
+WITH SEDAN_IDS AS (
+    SELECT
+        CAR_ID
+    FROM
+        CAR_RENTAL_COMPANY_CAR
+    WHERE
+        CAR_TYPE = '세단'
+)
+
+SELECT
+    DISTINCT(CAR_ID)
+FROM
+    CAR_RENTAL_COMPANY_RENTAL_HISTORY rh
+WHERE
+    ( rh.CAR_ID IN (
+        SELECT
+            CAR_ID
+        FROM
+            SEDAN_IDS
+    ) )
+AND
+    rh.START_DATE BETWEEN '2022-10-01' AND '2022-10-31'
+ORDER BY
+    CAR_ID DESC
