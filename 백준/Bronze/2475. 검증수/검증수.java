51cd3b952a983f57d9b861bfa66a0cd53548b031
junslog <wonjon0930@daum.net>
2024-03-09 16:04:09 +0900

    [Bronze V] Title: 검증수, Time: 236 ms, Memory: 18760 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .map(v -> v * v)
+                .sum() % 10);
+    }
+}
