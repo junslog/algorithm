fa825a86bd018efe6a60ee7d45d1c17ed18dd563
junslog <wonjon0930@daum.net>
2024-03-10 12:42:49 +0900

    [Bronze V] Title: N 찍기, Time: 164 ms, Memory: 17256 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
+        System.out.println(sb);
     }
 }
