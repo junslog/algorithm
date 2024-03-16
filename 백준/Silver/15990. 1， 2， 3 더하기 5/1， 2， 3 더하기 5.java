507ce2b9b3dd6c1cc414140682f0ec4bc5971c3c
junslog <wonjon0930@daum.net>
2024-03-16 17:03:47 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 112 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        long[][] dp = new long[100001][4];
+        dp[1][1] = 1;
+        dp[1][2] = 0;
+        dp[1][3] = 0;
+        dp[2][1] = 0;
+        dp[2][2] = 1;
+        dp[2][3] = 0;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        for (int i = 4; i <= 100000; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % 1000000009;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % 1000000009;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % 1000000009;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            int curr = Integer.parseInt(br.readLine());
+            sb.append((dp[curr][1] + dp[curr][2] + dp[curr][3]) % 1000000009).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
