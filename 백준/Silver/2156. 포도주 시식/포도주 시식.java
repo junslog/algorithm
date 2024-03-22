af702185c0d7835fcaf0ff1047ed421fd3e9aece
junslog <wonjon0930@daum.net>
2024-03-22 20:26:29 +0900

    [Silver I] Title: 포도주 시식, Time: 100 ms, Memory: 13168 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -11,33 +11,29 @@ public class Main {
             p[i] = Integer.parseInt(br.readLine());
         }
         int[][] dp = new int[N + 1][3];
-        // 1. 점화식 및 초기값 설정
-        // dp[N][1] = max(dp[N-2][1], dp[N-2][2]) + p[N]
-        // dp[N][2] = dp[N-1][1] + p[N]
+
         dp[1][0] = 0;
         dp[1][1] = p[1];
         dp[1][2] = 0;
         if (N >= 2) {
-            dp[2][0] = p[1];
-            dp[2][1] = p[2];
-            dp[2][2] = p[1] + p[2];
-        }
-
-        for (int i = 3; i <= N; i++) {
-            dp[i][0] = Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]);
-            dp[i][1] = dp[i - 1][0] + p[i];
-            dp[i][2] = dp[i - 1][1] + p[i];
-        }
-
-        int max = dp[1][1];
-        for (int i = 1; i <= N; i++) {
-            if (max < dp[i][1]) {
-                max = dp[i][1];
+            for (int i = 2; i <= N; i++) {
+                dp[i][0] = Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]);
+                dp[i][1] = dp[i - 1][0] + p[i];
+                dp[i][2] = dp[i - 1][1] + p[i];
             }
-            if (max < dp[i][2]) {
-                max = dp[i][2];
+
+            int max = dp[1][1];
+            for (int i = N - 2; i <= N; i++) {
+                if (max < dp[i][1]) {
+                    max = dp[i][1];
+                }
+                if (max < dp[i][2]) {
+                    max = dp[i][2];
+                }
             }
+            System.out.println(max);
+        } else {
+            System.out.println(p[1]);
         }
-        System.out.println(max);
     }
 }
