76e531d9218e88dac6761080d546f0292e833eba
junslog <wonjon0930@daum.net>
2024-03-31 14:20:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,6 +1,27 @@
 class Solution {
     public int solution(int num) {
-        int answer = 0;
-        return answer;
+        if(num == 1){
+            return 0;
+        }
+        boolean reachedOne = false;
+        int count = -1;
+        for(int i = 0; i < 500; i++){
+            
+            if(num % 2 == 0){
+                num /= 2;
+            } else if(num % 2 == 1){
+                num = num * 3 + 1;
+            }
+            
+            if(num == 1){
+                count = i + 1;
+                reachedOne = true;
+                break;
+            }
+        }
+        if(reachedOne){
+            return count;
+        }
+        return -1;
     }
 }
