761ef2c88676d01dbb96dfec98ae15d170ecbdb3
junslog <wonjon0930@daum.net>
2024-05-02 08:29:26 +0900

    [Bronze V] Title: 합, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((n * (n+1)) / 2);
+    }
+}
