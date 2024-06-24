0223131aedea85137fd448716fb3bc468dd1eab4
junslog <wonjon0930@daum.net>
2024-06-24 14:20:27 +0900

    [level 0] Title: ad 제거하기, Time: 2.34 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr)
+                     .filter(s -> !s.contains("ad"))
+                     .toArray(String[]::new);
+    }
+}
