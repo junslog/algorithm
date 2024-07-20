d0a40ddbbdc3a68e8a1a3bf41aafbc821c952525
junslog <wonjon0930@daum.net>
2024-07-21 00:52:44 +0900

    [level 0] Title: 배열 비교하기, Time: 1.51 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,18 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        if(arr1.length > arr2.length){
-            return 1;
-        } else if(arr1.length < arr2.length){
-            return -1;
-        } else {
-            int arr1Sum = Arrays.stream(arr1).sum();
-            int arr2Sum = Arrays.stream(arr2).sum();
-            if(arr1Sum == arr2Sum){
-                return 0;
-            }
-            return arr1Sum > arr2Sum ? 1 : -1;
-        }
+        int answer = 0;
+        return answer;
     }
 }
