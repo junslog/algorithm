ad7a615fbea89c0317b1f309341faf16357b7f20
junslog <wonjon0930@daum.net>
2024-09-11 23:17:22 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int counter = 0;
+        for(int i : num_list){
+            while(i > 1){
+                if(i % 2 == 0){
+                    i /= 2;
+                } else {
+                    i = (i-1)/2;
+                }
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
