91530367889a6ac20a7dfd4eb6df74a93627369d
junslog <wonjon0930@daum.net>
2024-09-14 09:03:05 +0900

    [level 3] Title: 등굣길, Time: 0.65 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,37 +1,6 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        
-        boolean[][] hasPuddle = new boolean[m+1][n+1];
-        long[][] steps = new long[m+1][n+1];
-        steps[1][1] = 1;
-        
-        for(int[] pos : puddles){
-            steps[pos[0]][pos[1]] = 0;
-            hasPuddle[pos[0]][pos[1]] = true;
-        }
-        
-        for(int i = 2; i <= m; i++){
-            if(hasPuddle[i][1]){
-                break;
-            }
-            steps[i][1] = 1;
-        }
-        
-        for(int i = 2; i <= n; i++){
-            if(hasPuddle[1][i]){
-                break;
-            }
-            steps[1][i] = 1;
-        }
-        
-        for(int i = 2; i <= m; i++){
-            for(int j = 2; j <= n; j++){
-                if(hasPuddle[i][j]){
-                    continue;
-                }
-                steps[i][j] = (steps[i][j-1] + steps[i-1][j]) % 1000000007;
-            }
-        }
-        return (int) steps[m][n];
+        int answer = 0;
+        return answer;
     }
 }
