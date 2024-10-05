3e4629e0ee9287d22e22f5b9ecbbeeeb1146fdb3
junslog <wonjon0930@daum.net>
2024-10-05 20:36:34 +0900

    [Silver II] Title: 나무 자르기, Time: 440 ms, Memory: 171760 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    long[] height = new long[N];
+    st = new StringTokenizer(br.readLine());
+    long maxHeight = Long.MIN_VALUE;
+    for (int i = 0; i < N; i++) {
+      height[i] = Integer.parseInt(st.nextToken());
+      maxHeight = Math.max(maxHeight, height[i]);
+    }
+    long left = 0L;
+    long right = maxHeight;
+    while (left <= right) {
+      long mid = (left + right + 1) / 2;
+      long sum = 0L;
+      for (int i = 0; i < N; i++) {
+        sum += (height[i] > mid ? height[i] - mid : 0);
+      }
+      if (sum >= M) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    System.out.println(right);
+  }
+
+}
