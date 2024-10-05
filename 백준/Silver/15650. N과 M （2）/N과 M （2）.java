6c2250ee9d1cbdb5c823e268573dc59d257569f8
junslog <wonjon0930@daum.net>
2024-10-05 19:02:32 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,41 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] items;
-    static boolean[] check;
-    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        check = new boolean[N + 1];
-        items = new int[M];
-        combination(0, 1);
-        System.out.println(sb);
-        br.close();
+  static int N, M;
+  static boolean[] visited;
+  static int[] values;
+
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    visited = new boolean[N + 1];
+    values = new int[N];
+    backtracking(1, 0);
+    System.out.println(sb);
+    br.close();
+  }
+
+  private static void backtracking(int start, int depth) {
+    if (depth == M) {
+      for (int i = 0; i < M; i++) {
+        sb.append(values[i]).append(" ");
+      }
+      sb.append("\n");
     }
 
-    public static void combination(int depth, int start) {
-        if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(items[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for (int i = start; i <= N; i++) {
-            if (check[i]) {
-                continue;
-            }
-            check[i] = true;
-            items[depth] = i;
-            combination(depth + 1, i + 1);
-            check[i] = false;
-        }
+    for (int i = start; i <= N; i++) {
+      if (visited[i]) {
+        continue;
+      }
+      visited[i] = true;
+      values[depth] = i;
+      backtracking(i + 1, depth + 1);
+      visited[i] = false;
     }
+  }
 }
