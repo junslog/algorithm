f0c1508997b535cacd8025834d2c155d28806275
junslog <wonjon0930@daum.net>
2024-10-05 21:10:43 +0900

    [Silver II] Title: 랜선 자르기, Time: 116 ms, Memory: 15256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,53 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        long[] lans = new long[K];
-        for (int i = 0; i < K; i++) {
-            lans[i] = Long.parseLong(br.readLine());
-        }
-        Arrays.sort(lans); // O(N^2) 까지 갈 수 있음.
-        long max = 0;
-        long from = 1;
-        long to = lans[lans.length - 1];
-        // Binary Search
-        while (true) {
-            long mid = (from + to) / 2;
-            if (to == from) {
-                max = from;
-                break;
-            }
-            int sum = 0;
-            for (int i = 0; i < K; i++) {
-                sum += lans[i] / mid;
-            }
-            if (sum >= N) {
-                from = mid;
-            } else {
-                to = mid;
-            }
 
-            if (to - from == 1) {
-                long sum2 = 0;
-                for (int i = 0; i < K; i++) {
-                    sum2 += lans[i] / to;
-                }
-                if (sum2 >= N) {
-                    max = to;
-                } else {
-                    max = from;
-                }
-                break;
-            }
-        }
-        System.out.println(max);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int K = Integer.parseInt(st.nextToken());
+    int N = Integer.parseInt(st.nextToken());
+    long[] lans = new long[K];
+    long min = 1L;
+    long max = Long.MIN_VALUE;
+    for (int i = 0; i < K; i++) {
+      lans[i] = Long.parseLong(br.readLine());
+      max = Math.max(lans[i], max);
     }
+    max += 1;
+
+    while (min < max) {
+      long mid = (min + max) / 2;
+      long count = 0L;
+      for (long l : lans) {
+        count += (l / mid);
+      }
+
+      if (count >= N) {
+        min = mid + 1;
+      } else {
+        max = mid;
+      }
+    }
+    System.out.println(min - 1);
+  }
 }
